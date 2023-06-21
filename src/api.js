c64e3c3abd0d9706d47b83174969b3dce8e91c87
magnifiq <124519076+magnifiq@users.noreply.github.com>
2023-06-21 19:34:07 +0300

    Add source code

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'c94b085d17msh6caa4490aeddfcbp14d15ejsn57a3927ef29f',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL="https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL="https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY="038189f3d03849b708bc9608fba3f2ac";
